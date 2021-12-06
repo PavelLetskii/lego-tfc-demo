1c892ba34b7b45ea114d5924eceff2e6e1c838aa
Pavel Letskii <pavel.letskii@nordcloud.com>
2021-12-06 13:58:58 +0200

    VPC module and variables

---
+++ vpc/variables.tf
@@ -0,0 +1,42 @@
+variable "vpc_name" {
+  default = "cluster_a_vpc"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "azs_list" {
+  type = list
+  default = [
+    "eu-north-1a",
+    "eu-north-1b",
+    "eu-north-1c"
+  ]
+}
+
+variable "private_subnets" {
+  type = list
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
+}
+
+variable "public_subnets" {
+  type = list
+  default = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24"
+  ]
+}
+
+variable "enable_nat_gw" {
+  default = true
+}
+
+variable "enable_vpc_gw" {
+  default = true
+}
---
+++ vpc/vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.azs_list
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = var.enable_nat_gw
+  enable_vpn_gateway = var.enable_vpn_gw
+
+  tags = {
+    Name = var.vpc_name
+    Environment = "dev"
+  }
+}
