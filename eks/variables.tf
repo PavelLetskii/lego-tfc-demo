8b63e52b2a8852d6b43c3181c26e2ba9d89f0438
Pavel Letskii <pavel.letskii@nordcloud.com>
2021-12-06 16:15:26 +0200

    Add eks code

---
+++ eks/variables.tf
@@ -0,0 +1,15 @@
+variable "cluster_version" {
+  default = "1.21"
+}
+
+variable "cluster_name" {
+  default = "lego-demo-cluster-a"
+}
+
+variable "instance_type" {
+  default = "m4.large"
+}
+
+variable "asg_max_size" {
+  default = 5
+}
